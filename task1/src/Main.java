a95f1224cc88bb52b840df27d4317e6a8ad94608
AlekseyShugol <aleksshugol2911@gmail.com>
2024-04-24 11:14:48 +0300

    добавлен task1

---
+++ task1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Aвтор:\tСтивен Кинг");
+        System.out.println("Книги:\tЦикл романов \"Тёмная башня\"");
+    }
+}
