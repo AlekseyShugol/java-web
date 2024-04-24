727a291bd74f8585cd89059b622ae5d43402fa74
AlekseyShugol <aleksshugol2911@gmail.com>
2024-04-24 10:48:16 +0300

    task1 deleted

--- task1/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Aвтор:\tСтивен Кинг");
-        System.out.println("Книги:\tЦикл романов \"Тёмная башня\"");
-    }
-}
